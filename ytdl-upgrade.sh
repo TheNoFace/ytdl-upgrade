ab2504e32ea38c784e1fa8f5ac1ab8d2a48e0699
TheNoFace <thenoface303@gmail.com>
2021-03-06 16:17:49 +0900

    Install youtube-dl if it's not present
    ...and many other changes

--- ytdl-upgrade.sh
+++ ytdl-upgrade.sh
@@ -1,40 +1,73 @@
 #!/bin/bash
 
-[[ $(id -u) -ne 0 ]] && echo "Please run as root!" && exit 255 # https://stackoverflow.com/questions/18215973/how-to-check-if-running-as-root-in-a-bash-script
-[[ -z "$(command -v youtube-dl)" ]] && echo "youtube-dl not found!" && exit 1
+pkgName='youtube-dl'
+
+[[ $(id -u) -ne 0 ]] && echo "Please run as root!" && exit 255 # https://stackoverflow.com/a/28776100
+
+function installCheck()
+{
+	installedPath=$(command -v $pkgName)
+	if [ -z $installedPath ]
+	then
+		echo -en "$pkgName not found!\nDo you want to install $pkgName? (Y/N): "
+		read wishTo
+
+		if [[ "$wishTo" = 'Y' || "$wishTo" = 'y' || "$wishTo" = 'YES' || "$wishTo" = 'Yes' || "$wishTo" = 'yes' ]]
+		then
+			getPkg
+			echo -e "Installed $pkgName (Version: $($pkgName --version))"
+			exit 0
+		else
+			echo "$pkgName installation cancelled!"
+			exit 0
+		fi
+	else
+		echo "$pkgName is installed at $installedPath"
+	fi
+}
+
+function getPkg()
+{
+	echo -en "Downloading $pkgName..."
+	curl -sL https://yt-dl.org/downloads/latest/$pkgName -o /usr/local/bin/$pkgName
+	echo -en "\rDownloading $pkgName... Done!\nSetting permission..."
+	chmod a+rx /usr/local/bin/$pkgName
+	echo -e "\rSetting permission... Done!"
+}
 
 function versionCheck()
 {
 	echo -en "Checking version..."
-	currentVersion=$(youtube-dl --version)
-	newVersion=$(curl -s https://raw.githubusercontent.com/ytdl-org/youtube-dl/master/ChangeLog | awk 'FNR == 1 {print $2}')
+	currentVersion=$($pkgName --version)
+	newVersion=$(curl -s https://raw.githubusercontent.com/ytdl-org/$pkgName/master/ChangeLog | awk 'FNR == 1 {print $2}')
 
 	if [ $currentVersion != $newVersion ]
 	then
 		isUpdated=0
-		echo -e "\rChecking version... ($currentVersion / $newVersion)"
-		echo -en "Found new version, updating..."
-		ytdlUpdate
+		echo -e "\rChecking version... ($currentVersion -> $newVersion)\nFound new version, updating..."
+		pkgUpdate
 	else
 		if [ "$isUpdated" = 1 ]
 		then
 			echo -e "\nUpdate finished ($currentVersion)"
+			exit 0
 		else
-			echo -e "\nyoutube-dl version is up to date ($currentVersion)"
+			echo -e "\n$pkgName version is up to date ($currentVersion)"
+			exit 1
 		fi
 	fi
 }
 
-function ytdlUpdate()
+function pkgUpdate()
 {
-	curl -sL https://yt-dl.org/downloads/latest/youtube-dl -o /usr/bin/youtube-dl
-	chmod a+rx /usr/bin/youtube-dl
+	getPkg
 	isUpdated=1
-	echo -e "\rFound new version, updating... Done!"
 	versionCheck
 }
 
 ###
 
+installCheck
 versionCheck
-exit 0
+
+echo -e '\nERROR: EOF' && exit 10
